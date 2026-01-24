c8f4fbc8225a00cfce475c910b0f0f4e3a646481
Dave Oliverio <davezer27@gmail.com>
2026-01-23 22:09:24 -0500

    pathname is wrong

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 
 <svelte:head>
   <!-- 16x16 tab icon -->
-  <link rel="icon" type="image/png" sizes="16x16" href="/events/Favicon 16 x16.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/Favicon 16 x16.png" />
 
   <!-- (optional but recommended) also provide 32x32 so browsers choose best -->
-  <link rel="icon" type="image/png" sizes="32x32" href="/events/Favicon 32 x32.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/Favicon 32 x32.png" />
 </svelte:head>
 
 {@render children()}

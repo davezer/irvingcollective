75ba553638eba787d10a8388b25bc2abc9a5b50b
Dave Oliverio <davezer27@gmail.com>
2026-01-23 22:04:06 -0500

    updated logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-	import '../app.css';
-
-	let { children } = $props();
+  import '../app.css';
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <!-- 16x16 tab icon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="/events/Favicon 16 x16.png" />
+
+  <!-- (optional but recommended) also provide 32x32 so browsers choose best -->
+  <link rel="icon" type="image/png" sizes="32x32" href="/events/Favicon 32 x32.png" />
 </svelte:head>
 
 {@render children()}
